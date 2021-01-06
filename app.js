a72a6ab8b9d76605f06b4a8d2176a1e5a41f558e
lope <lope@lopedeMac-mini.lan>
2021-01-06 21:58:03 +0800

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello LOPE,Welcome to love and peace!'));
+app.get('/', (req, res) => res.send('Hello LOPE,这是一个全流程测试。'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
